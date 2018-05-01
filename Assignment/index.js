fb70444cef9da09d066b2767eb2c068ccc020fb0
Ghada Messaoudi <ghada.messaoudi@seattlecentral.edu>
2018-04-30 18:10:09 -0700

    Adds eslint

--- Assignment/index.js
+++ Assignment/index.js
@@ -1,78 +1,79 @@
-var http = require("http");
-var booksModule = require("./module.js");
+let http = require("http")
+let booksModule = require("./module.js")
 
-http.createServer(function (req, res) {
-  var fs = require("fs");
-  var path = req.url.toLowerCase();
-  if (path == '/home') {
-    path = '/';
-  }
-  path = path.split("?")
-  route = path[0]
-  switch (route) {
+http.createServer((req, res) => {
+	let fs = require("fs")
+	let path = req.url.toLowerCase()
+	if (path == "/home") {
+		path = "/"
+	}
+	path = path.split("?")
+	let route = path[0]
+	switch (route) {
 
-    case '/':
-      fs.readFile("public/home.html", function (err, data) {
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        res.write(data);
-        res.end();
-      });
-      break;
+	case "/":
+		fs.readFile("public/home.html", (err, data) => {
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			})
+			res.write(data)
+			res.end()
+		})
+		break
 
-    case '/about':
-      fs.readFile("public/about.html", function (err, data) {
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        res.write(data);
-        res.end();
-      });
-      break;
+	case "/about":
+		fs.readFile("public/about.html", (err, data) => {
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			})
+			res.write(data)
+			res.end()
+		})
+		break
 
-    case '/get':
-      var isbn = path[1].split("=")[1]
-      var book = JSON.parse(booksModule.getB(isbn))
-      if (book.msg == undefined) {
-        res.writeHead(200, {
-          'Content-Type': 'application/json'
-        });
-        res.write('Searching for ' + isbn + ' : \n' + book.title + " : " + book.description);
-      } else {
-        res.writeHead(404, {
-          'Content-Type': 'application/json'
-        });
-        res.write('Searching for ' + isbn + ' : \n' + book.msg);
-      }
-      res.end();
-      break;
-
-    case '/delete':
-      var isbn = path[1].split("=")[1]
-      var result = JSON.parse(booksModule.deleteB(isbn))
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-      if (result.success) {
-        res.end(result.title + " removed.");
-      } else {
-        res.end("Not Found");
-      }
-      break;
-
-    case '/getall':
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      res.end(booksModule.getAll())
-      break;
-
-    default:
-      res.writeHead(404, {
-        'Content-Type': 'text/html'
-      });
-      res.end('<br><br><br><br><h1>Page Not Found</h1>');
-      break;
-  }
-}).listen(process.env.PORT || 3000);
+	case "/get": {
+		let isbn = path[1].split("=")[1]
+		let book = JSON.parse(booksModule.getB(isbn))
+		if (book.msg == undefined) {
+			res.writeHead(200, {
+				"Content-Type": "application/json"
+			})
+			res.write("Searching for " + isbn + " : \n" + book.title + " : " + book.description)
+		} else {
+			res.writeHead(404, {
+				"Content-Type": "application/json"
+			})
+			res.write("Searching for " + isbn + " : \n" + book.msg)
+		}
+		res.end()
+		break
+	}
+	case "/delete": {
+		let isbn = path[1].split("=")[1]
+		let result = JSON.parse(booksModule.deleteB(isbn))
+		res.writeHead(200, {
+			"Content-Type": "text/plain"
+		})
+		if (result.success) {
+			res.end(result.title + " removed.")
+		} else {
+			res.end("Not Found")
+		}
+		break
+	}
+	case "/getall": {
+		res.writeHead(200, {
+			"Content-Type": "application/json"
+		})
+		res.end(booksModule.getAll())
+		break
+	}
+	default: {
+		res.writeHead(404, {
+			"Content-Type": "text/html"
+		})
+		res.end("<br><br><br><br><h1>Page Not Found</h1>")
+		break
+	}
+	}
+}).listen(process.env.PORT || 3000)
--- Assignment/module.js
+++ Assignment/module.js
@@ -1,87 +1,87 @@
-var books = [{
-        isbn: "9781593275846",
-        title: "Eloquent JavaScript, Second Edition",
-        subtitle: "A Modern Introduction to Programming",
-        author: "Marijn Haverbeke",
-        pages: 472,
-        description: "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications."
-    },
-    {
-        isbn: "9781449325862",
-        title: "Git Pocket Guide",
-        subtitle: "A Working Introduction",
-        author: "Richard E. Silverman",
-        pages: 234,
-        description: "This pocket guide is the perfect on-the-job companion to Git, the distributed version control system. It provides a compact, readable introduction to Git for new users, as well as a reference to common commands and procedures for those of you with Git experience."
-    },
-    {
-        isbn: "9781593277574",
-        title: "Understanding ECMAScript 6",
-        subtitle: "The Definitive Guide for JavaScript Developers",
-        author: "Nicholas C. Zakas",
-        pages: 352,
-        description: "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript."
-    },
-    {
-        isbn: "9781449331818",
-        title: "Learning JavaScript Design Patterns",
-        subtitle: "A JavaScript and jQuery Developer's Guide",
-        author: "Addy Osmani",
-        pages: 254,
-        description: "With Learning JavaScript Design Patterns, you'll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you."
-    },
-    {
-        isbn: "9781449365035",
-        title: "Speaking JavaScript",
-        subtitle: "An In-Depth Guide for Programmers",
-        author: "Axel Rauschmayer",
-        pages: 460,
-        "description": "Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position."
-    }
+let books = [{
+	isbn: "9781593275846",
+	title: "Eloquent JavaScript, Second Edition",
+	subtitle: "A Modern Introduction to Programming",
+	author: "Marijn Haverbeke",
+	pages: 472,
+	description: "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications."
+},
+{
+	isbn: "9781449325862",
+	title: "Git Pocket Guide",
+	subtitle: "A Working Introduction",
+	author: "Richard E. Silverman",
+	pages: 234,
+	description: "This pocket guide is the perfect on-the-job companion to Git, the distributed version control system. It provides a compact, readable introduction to Git for new users, as well as a reference to common commands and procedures for those of you with Git experience."
+},
+{
+	isbn: "9781593277574",
+	title: "Understanding ECMAScript 6",
+	subtitle: "The Definitive Guide for JavaScript Developers",
+	author: "Nicholas C. Zakas",
+	pages: 352,
+	description: "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript."
+},
+{
+	isbn: "9781449331818",
+	title: "Learning JavaScript Design Patterns",
+	subtitle: "A JavaScript and jQuery Developer's Guide",
+	author: "Addy Osmani",
+	pages: 254,
+	description: "With Learning JavaScript Design Patterns, you'll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you."
+},
+{
+	isbn: "9781449365035",
+	title: "Speaking JavaScript",
+	subtitle: "An In-Depth Guide for Programmers",
+	author: "Axel Rauschmayer",
+	pages: 460,
+	"description": "Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position."
+}
 ]
 
 exports.getAll = () => {
-    return books;
-};
+	return books
+}
 
 exports.getB = (isbn) => {
-    var b = books.find(function (book) {
-        return book.isbn == isbn;
-    })
-    if (b == null) {
-        return {
-            success: false
-        }
-    }
-    return {
-        success: true,
-        book: b
-    };
-};
+	let b = books.find((book) => {
+		return book.isbn == isbn
+	})
+	if (b == null) {
+		return {
+			success: false
+		}
+	}
+	return {
+		success: true,
+		book: b
+	}
+}
 
 exports.deleteB = (isbn) => {
-    var bookD = books.find(function (book) {
-        if (book.isbn == isbn) {
-            return book
-        }
-    })
-    if (bookD == null) {
-        return {
-            success: false,
-            length: books.length
-        }
-    } else {
-        var index = books.indexOf(bookD);
-        if (index > -1) {
-            books.splice(index, 1);
-        }
-        return {
-            success: true,
-            title: bookD.title,
-            length: books.length
-        };
-    }
-};
+	let bookD = books.find((book) => {
+		if (book.isbn == isbn) {
+			return book
+		}
+	})
+	if (bookD == null) {
+		return {
+			success: false,
+			length: books.length
+		}
+	} else {
+		let index = books.indexOf(bookD)
+		if (index > -1) {
+			books.splice(index, 1)
+		}
+		return {
+			success: true,
+			title: bookD.title,
+			length: books.length
+		}
+	}
+}
 
 // exports.deleteB = (isbn) => {
 //     var result = books.find(function (book,index) {
